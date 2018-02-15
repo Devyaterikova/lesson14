abc9d8369ac583f98d7d08022b3c39c6ea5034ec
Elena Devyaterikova <A1978D@yandex.ru>
2018-02-15 12:38:09 +0300

    Домашнее задание к лекции 4.3 «SELECT из нескольких таблиц»

---
+++ dbconfig.php
@@ -0,0 +1,14 @@
+<?php
+//$servername = "localhost";
+//$dbname = "ptrofimov";
+//$username = "ptrofimov";
+//$password = "neto1216";
+//$cookie_expiration_time = time() + 60 * 15;
+
+$servername = "localhost";
+$dbname = "edevyaterikova";
+$username = "edevyaterikova";
+$password = "neto1390";
+$cookie_expiration_time = time() + 60 * 15;
+
+?>
